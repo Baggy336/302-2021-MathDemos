fbfa5a53cb2456ecf7d32c36cf1af0236eddcb5f
Erik Howley <howleye@ferris.edu>
2021-02-11 18:31:06 -0500

    Week 5 Quaternion Lerp/Slide

--- AnimDemos/Assets/Scripts/SlideTowards.cs
+++ AnimDemos/Assets/Scripts/SlideTowards.cs
@@ -5,9 +5,24 @@ using UnityEngine;
 public class SlideTowards : MonoBehaviour
 {
     public Transform target;
+    private Camera cam;
 
+    void Start()
+    {
+        cam = GetComponentInChildren<Camera>();
+    }
     void Update()
     {
-        transform.position = AnimMath.Slide(transform.position, target.position, .01f);  
+        // Slides position towards the target
+        transform.position = AnimMath.Slide(transform.position, cam.transform.position, .01f);
+
+        // Get the vector to the target
+        Vector3 vectorToTarget = target.position - transform.position;
+
+        // Create the desired rotation
+        Quaternion targetRotation = Quaternion.LookRotation(vectorToTarget, Vector3.up);
+
+        // Slide camera rotation to look at the target
+        cam.transform.rotation = AnimMath.Slide(cam.transform.rotation, targetRotation, .05f);
     }
 }
